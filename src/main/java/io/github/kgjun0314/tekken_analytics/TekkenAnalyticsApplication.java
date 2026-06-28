baafc5e3a067898f16343d418151fc5fb63cdb7c
kgjun0314 <unicorn9717@gmail.com>
2026-06-28 17:40:06 +0900

    chore: initialize project

---
+++ src/main/java/io/github/kgjun0314/tekken_analytics/TekkenAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package io.github.kgjun0314.tekken_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekkenAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekkenAnalyticsApplication.class, args);
+	}
+
+}
