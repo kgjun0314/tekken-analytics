693cb89f7952d3e7d6302d288531c25e1923b45b
kgjun0314 <unicorn9717@gmail.com>
2026-07-04 21:07:47 +0900

    feat: add repository call metrics with AOP

--- src/main/java/io/github/kgjun0314/tekken_analytics/benchmark/RepositoryMetricsAspect.java
+++ src/main/java/io/github/kgjun0314/tekken_analytics/benchmark/RepositoryMetricsAspect.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,24 +18,27 @@ public class RepositoryMetricsAspect {
 
     private final RepositoryMetrics metrics;
 
-    @Before("""
-            execution(* io.github.kgjun0314.tekken_analytics..repository.*.*(..))
-            """)
-    public void countRepositoryCall(
-            JoinPoint joinPoint
-    ) {
+    @Before("execution(* io.github.kgjun0314.tekken_analytics..repository..*(..))")
+    public void countRepositoryCall(JoinPoint joinPoint) {
 
-        String repository =
-                joinPoint.getSignature()
-                        .getDeclaringType()
-                        .getSimpleName();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
-        String method =
-                joinPoint.getSignature()
-                        .getName();
+        Class<?> repositoryClass = signature.getMethod().getDeclaringClass();
 
-        metrics.increment(
-                repository + "." + method
-        );
+        if (repositoryClass.equals(CrudRepository.class)
+                || repositoryClass.equals(JpaRepository.class)) {
+
+            repositoryClass = Arrays.stream(joinPoint.getTarget().getClass().getInterfaces())
+                    .filter(it -> it.getSimpleName().endsWith("Repository"))
+                    .findFirst()
+                    .orElse(repositoryClass);
+        }
+
+        String key =
+                repositoryClass.getSimpleName()
+                        + "."
+                        + signature.getMethod().getName();
+
+        metrics.increment(key);
     }
 }
