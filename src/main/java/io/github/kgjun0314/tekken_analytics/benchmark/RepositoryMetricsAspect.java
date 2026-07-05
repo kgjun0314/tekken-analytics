e3b55a5410e10e6a26e0741ef975b61bc0a7d662
kgjun0314 <unicorn9717@gmail.com>
2026-07-05 20:10:06 +0900

    refactor: measure repository metrics only on implementation classes

--- src/main/java/io/github/kgjun0314/tekken_analytics/benchmark/RepositoryMetricsAspect.java
+++ src/main/java/io/github/kgjun0314/tekken_analytics/benchmark/RepositoryMetricsAspect.java
@@ -4,12 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Aspect
 @Component
@@ -18,34 +13,19 @@ public class RepositoryMetricsAspect {
 
     private final RepositoryMetrics metrics;
 
-    @Around("execution(* io.github.kgjun0314.tekken_analytics..repository..*(..))")
+    @Around("""
+    execution(* io.github.kgjun0314.tekken_analytics..repository..*RepositoryImpl.*(..))
+    """)
     public Object measureRepositoryCall(
             ProceedingJoinPoint joinPoint
     ) throws Throwable {
 
-        MethodSignature signature =
-                (MethodSignature) joinPoint.getSignature();
-
-        Class<?> repositoryClass =
-                signature.getMethod().getDeclaringClass();
-
-        if (repositoryClass.equals(CrudRepository.class)
-                || repositoryClass.equals(JpaRepository.class)) {
-
-            repositoryClass = Arrays.stream(
-                            joinPoint.getTarget()
-                                    .getClass()
-                                    .getInterfaces()
-                    )
-                    .filter(it -> it.getSimpleName().endsWith("Repository"))
-                    .findFirst()
-                    .orElse(repositoryClass);
-        }
+        Class<?> repositoryClass = joinPoint.getTarget().getClass();
 
         String key =
                 repositoryClass.getSimpleName()
                         + "."
-                        + signature.getMethod().getName();
+                        + joinPoint.getSignature().getName();
 
         long start = System.nanoTime();
 
